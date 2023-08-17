0cbab84d1ba27df0903e9ddba1f6660781ee6051
Сергей Лющин <xvid1231@yandex.ru>
2023-08-17 18:09:05 +0300

    second_check

--- backend/recipe/admin.py
+++ backend/recipe/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Recipe
 
-# Register your models here.
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'author', 'cooking_time', 'total_favorites')
+    list_filter = ('name', 'author', 'tags')
+    search_fields = ('name', 'author__username', 'author__email')
+
+    def total_favorites(self, obj):
+        return obj.favorited_by.count()
+    total_favorites.short_description = 'Total Favorites'
